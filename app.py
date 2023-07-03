1359edcfc7ec57e3c2c4ea10b2f48f46693e1634
eugeneleychenko <nycspicebo@gmail.com>
2023-07-03 10:58:02 -0400

    init

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from langchain.agents import create_csv_agent
+from langchain.llms import OpenAI
+from langchain.agents import AgentType
+from langchain.chat_models import ChatOpenAI
+from dotenv import load_dotenv
+import pandas as pd
+from io import StringIO
+
+
+
+def main():
+    load_dotenv()
+    st.set_page_config("Ask your CSV Anything!")
+    st.title("Ask your CSV Anything!")
+    csv_upload = st.file_uploader("Upload your CSV!", type="csv")
+    st.write(csv_upload)
+    
+    
+
+    if csv_upload:
+            # stringio = StringIO(csv_upload.getvalue().decode("utf-8"))
+            question  = st.text_input("What would you like to know about:")
+            llm = ChatOpenAI(temperature=0)
+            agent = create_csv_agent(
+            llm, csv_file = csv_upload, agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)  # Pass file name to function
+
+            if csv_upload is not None and question != "":
+                agent.run(question)
+        
+
+
+
+if __name__ == '__main__':
+    main()
