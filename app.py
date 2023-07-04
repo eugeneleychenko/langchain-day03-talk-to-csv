c75852ac06146fabb1aa7357855eaaa4333db4b4
eugeneleychenko <nycspicebo@gmail.com>
2023-07-03 20:32:02 -0400

    final

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from langchain.chat_models import ChatOpenAI
 from dotenv import load_dotenv
 import pandas as pd
 from io import StringIO
+import time
 
 
 
@@ -14,21 +15,28 @@ def main():
     st.set_page_config("Ask your CSV Anything!")
     st.title("Ask your CSV Anything!")
     csv_upload = st.file_uploader("Upload your CSV!", type="csv")
-    st.write(csv_upload)
+    
+    # st.write(df)
     
     
     
 
     if csv_upload:
-            # stringio = StringIO(csv_upload.getvalue().decode("utf-8"))
-            question  = st.text_input("What would you like to know about:")
-            llm = ChatOpenAI(temperature=0)
-            agent = create_csv_agent(
-            llm=llm, path=csv_upload.name, agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)  # Pass file name to function
+        # stringio = StringIO(csv_upload.getvalue().decode("utf-8"))
+        df = pd.read_csv(csv_upload)
+        question  = st.text_input("What would you like to know about:")
+        llm = ChatOpenAI(temperature=0, model="gpt-4")
+        agent = create_pandas_dataframe_agent(
+        llm=llm, df=df, agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)  # Pass file name to function
 
-            if csv_upload is not None and question != "":
-                answer = agent.run(question)
-                st.write(answer)
+        
+        if csv_upload is not None and question != "":
+            with st.spinner('Thinking'):
+                time.sleep(5)
+            answer = agent.run(question)
+            with st.spinner('Looking for answers'):
+                time.sleep(5)
+            st.write(answer)
         
 
 
